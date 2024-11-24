585bd16ffa7e6d600c898689db4ef7dd43608f5f
Raphael <raphael.aldaz@gmail.com>
2024-11-24 17:45:23 +0100

    init project

---
+++ MyHotelsAPI/MyHotels/src/main/java/raphael/MyHotels/MyHotelsApplication.java
@@ -0,0 +1,13 @@
+package raphael.MyHotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHotelsApplication.class, args);
+	}
+
+}
---
+++ MyHotelsAPI/MyHotels/src/test/java/raphael/MyHotels/MyHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package raphael.MyHotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
