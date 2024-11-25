87961f5af4332f47b79c7fe9e2fa1365f72fd0bd
Raphael <raphael.aldaz@gmail.com>
2024-11-25 14:59:36 +0100

    making picture directory

--- MyHotelsAPI/MyHotels/src/main/java/raphael/MyHotels/MyHotelsApplication.java
+++ MyHotelsAPI/MyHotels/src/main/java/raphael/MyHotels/MyHotelsApplication.java
@@ -1,13 +1,33 @@
 package raphael.MyHotels;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
+@Slf4j
 public class MyHotelsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyHotelsApplication.class, args);
+		makeDirectory();
+	}
+
+	private static void makeDirectory(){
+		File directory = new File(System.getProperty("user.dir"));
+		String path = "pictures";
+		File newDirectory = new File(directory, path);
+		if(!newDirectory.exists()){
+			boolean directoryStatus = newDirectory.mkdir();
+			if (directoryStatus){
+				log.info("Directory created");
+			} else {
+				log.warn("Error creating directory");
+			}
+		}
 	}
 
 }
